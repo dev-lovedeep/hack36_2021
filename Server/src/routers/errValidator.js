046a667de8c40f07a8b4d942ed38e0c09d77073f
developer-ishan <ishan@mnnit.ac.in>
2021-04-10 18:42:53 +0530

    validation bug removed

--- Server/src/routers/errValidator.js
+++ Server/src/routers/errValidator.js
@@ -2,6 +2,7 @@ var { validationResult } = require("express-validator");
 
 // handling errors thrown by express validator
 exports.errHandler = (req, res, next) => {
+  const errors = validationResult(req).array();
   console.log(errors);
   // checking for errors
   if (errors.length !== 0) {
