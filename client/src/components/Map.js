82223d4eb99512c71c84b5aadaeb58f67f147366
akstron <62210712+akstron@users.noreply.github.com>
2021-04-10 12:49:43 +0530

    Map connection to socket completed

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,9 +1,10 @@
 import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
+import icon from "../img/ambulance.png";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API;
 
-const Map = ({ lng, setLng, lat, setLat, zoom, setZoom }) => {
+const Map = ({ lng, setLng, lat, setLat, zoom, setZoom, socket }) => {
   const mapContainerRef = useRef(null);
 
   useEffect(() => {
@@ -15,6 +16,62 @@ const Map = ({ lng, setLng, lat, setLat, zoom, setZoom }) => {
     });
 
     map.addControl(new mapboxgl.NavigationControl(), "top-right");
+
+    map.loadImage(icon, (e, icon) => {
+      if (e) throw e;
+      map.addImage("icon", icon);
+    });
+
+    socket.on("driversLocation", (res) => {
+      // setambulances(ambulances);
+
+      if (!res) return;
+
+      const ambulances = {
+        type: "FeatureCollection",
+        features: [],
+      };
+
+      res.ambulances.forEach((ambulance) => {
+        const coords = [
+          ambulance.location.longitude,
+          ambulance.location.latitude,
+        ];
+
+        ambulances.features.push({
+          type: "Feature",
+          properties: {},
+          geometry: {
+            type: "Point",
+            coordinates: coords,
+            properties: {
+              icon: icon,
+            },
+          },
+        });
+      });
+
+      // Adding ambulance layer on map
+
+      if (map.getLayer("ambulances")) {
+        map.getSource("ambulances").setData(ambulances);
+      } else {
+        map.addLayer({
+          id: "ambulances",
+          type: "symbol",
+          source: {
+            type: "geojson",
+            data: ambulances,
+          },
+          layout: {
+            "icon-image": "icon",
+            "icon-allow-overlap": true,
+          },
+        });
+      }
+
+      console.log(res.ambulances);
+    });
 
     return () => map.remove();
   }, [lat, lng]);
